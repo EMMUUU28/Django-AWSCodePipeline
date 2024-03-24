55216b28b622fcaf66cde3e118a22c394cbd7dd0
EMMUUU28 <francisgudinho1234@gmail.com>
2024-03-25 02:10:21 +0530

    changed configuration files-ip

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["43.205.216.211"]/' /home/ubuntu/Django-AWSCodePipeline/Project/settings.py
+sed -i 's/\[]/\["13.233.207.182"]/' /home/ubuntu/Django-AWSCodePipeline/Project/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
