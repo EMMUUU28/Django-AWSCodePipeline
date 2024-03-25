2edcbdb574f7909f0a508133b812aa2c7765b5d8
EMMUUU28 <francisgudinho1234@gmail.com>
2024-03-25 13:49:01 +0530

    changed configuration files-dependencies2.0

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-virtualenv -p /usr/bin/python3.11 myenv
+virtualenv -p /usr/bin/python3.9 /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/Django-AWSCodePipeline/requirements.txt
